9cfddf0dc84ebbff8a0e515f04a0a0b9c3545f38
Kirill Popov <popov.kirill.a@yandex.ru>
2025-06-23 23:30:27 +0300

    Исправлен index.html, добавлен скрипт Telegram в index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Импортируем Telegram WebApp SDK
+const script = document.createElement('script');
+script.src = 'https://telegram.org/js/telegram-web-app.js';
+script.async = true;
+document.body.appendChild(script);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
