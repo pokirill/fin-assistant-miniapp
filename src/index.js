0f8918d69d5758b3eb6c06391731685389dc10ac
Kirill Popov <popov.kirill.a@yandex.ru>
2025-06-23 23:47:44 +0300

    Добавил основной код React-приложения

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
-
-// Импортируем Telegram WebApp SDK
-const script = document.createElement('script');
-script.src = 'https://telegram.org/js/telegram-web-app.js';
-script.async = true;
-document.body.appendChild(script);
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
